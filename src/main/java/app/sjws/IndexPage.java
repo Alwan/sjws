1c3d3dbf28a9984aece5afa014535961ef4e248e
Alwan <alwan.sa@gmail.com>
2021-10-07 19:10:48 +0300

    strong

---
+++ src/main/java/app/sjws/IndexPage.java
@@ -0,0 +1,14 @@
+package app.sjws;
+
+public class IndexPage {
+    private String greetings;
+
+
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
+    }
+
+    public String getGreetings() {
+        return greetings;
+    }
+}
